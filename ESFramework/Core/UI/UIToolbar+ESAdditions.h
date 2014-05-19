27d53730726a47b99d1c0a7296a39338a84859d2
Elf Sundae <elf.sundae@gmail.com>
2014-05-19 16:16:26 +0800

    UIToolBar+ESAdditons

---
+++ ESFramework/Core/UI/UIToolbar+ESAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIToolbar+ESAdditions.h
+//  ESFramework
+//
+//  Created by Elf Sundae on 5/19/14.
+//  Copyright (c) 2014 www.0x123.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIToolbar (ESAdditions)
+- (UIBarButtonItem *)itemWithTag:(NSInteger)tag;
+- (void)replaceItemWithTag:(NSInteger)tag withItem:(UIBarButtonItem *)newItem;
+
+@end
---
+++ ESFramework/Core/UI/UIToolbar+ESAdditions.m
@@ -0,0 +1,35 @@
+//
+//  UIToolbar+ESAdditions.m
+//  ESFramework
+//
+//  Created by Elf Sundae on 5/19/14.
+//  Copyright (c) 2014 www.0x123.com. All rights reserved.
+//
+
+#import "UIToolbar+ESAdditions.h"
+#import "NSArray+ESAdditions.h"
+
+@implementation UIToolbar (ESAdditions)
+
+- (UIBarButtonItem *)itemWithTag:(NSInteger)tag
+{
+        for (UIBarButtonItem *item in self.items) {
+                if (item.tag == tag) {
+                        return item;
+                }
+        }
+        return nil;
+}
+
+- (void)replaceItemWithTag:(NSInteger)tag withItem:(UIBarButtonItem *)newItem
+{
+        UIBarButtonItem *item = [self itemWithTag:tag];
+        if (item) {
+                NSMutableArray *newItems = [NSMutableArray arrayWithArray:self.items];
+                [newItems replaceObject:item withObject:newItems];
+                self.items = newItems;
+        }
+}
+
+
+@end
