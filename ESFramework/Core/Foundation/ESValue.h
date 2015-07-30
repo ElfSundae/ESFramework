b4aa02629825ae925207168ef84db1e5cd2f738a
Elf Sundae <elf.sundae@gmail.com>
2015-07-30 17:17:26 +0800

    add comment

--- ESFramework/Core/Foundation/ESValue.h
+++ ESFramework/Core/Foundation/ESValue.h
@@ -23,17 +23,13 @@ ES_EXTERN long long ESLongLongValue(id obj);
 ES_EXTERN unsigned long long ESULongLongValue(id obj);
 ES_EXTERN float ESFloatValue(id obj);
 ES_EXTERN double ESDoubleValue(id obj);
-/**
- * Note: if obj is not NSString nor NSNumber, return NO by default.
- * @see ESBoolVal()
- *
- * If obj is a NSString, skips initial space characters (whitespaceSet), or optional -/+ sign followed by zeroes.
- * Returns YES on encountering one of "Y", "y", "T", "t", or a digit 1-9. It ignores any trailing characters.
- */
 ES_EXTERN BOOL ESBoolValue(id obj);
-ES_EXTERN NSString *ESStringValue(id obj); // if obj is not NSString nor NSNumber, return nil.
-ES_EXTERN NSURL *ESURLValue(id obj); // if obj is not NSString nor NSURL, return nil.
+ES_EXTERN NSString *ESStringValue(id obj); // if obj is not NSString or NSNumber, return nil.
+ES_EXTERN NSURL *ESURLValue(id obj); // if obj is not NSString or NSURL, return nil.
 
+/**
+ * Deprecated
+ */
 
 ES_EXTERN BOOL ESIntVal(int *var, id obj);
 ES_EXTERN BOOL ESUIntVal(unsigned int *var, id obj);
@@ -45,10 +41,6 @@ ES_EXTERN BOOL ESLongLongVal(long long *var, id obj);
 ES_EXTERN BOOL ESULongLongVal(unsigned long long *var, id obj);
 ES_EXTERN BOOL ESFloatVal(float *var, id obj);
 ES_EXTERN BOOL ESDoubleVal(double *var, id obj);
-/**
- * If obj is a NSString, skips initial space characters (whitespaceSet), or optional -/+ sign followed by zeroes.
- * Returns YES on encountering one of "Y", "y", "T", "t", or a digit 1-9. It ignores any trailing characters.
- */
 ES_EXTERN BOOL ESBoolVal(BOOL *var, id obj);
 ES_EXTERN BOOL ESStringVal(NSString **var, id obj);
 ES_EXTERN BOOL ESURLVal(NSURL **var, id obj);
