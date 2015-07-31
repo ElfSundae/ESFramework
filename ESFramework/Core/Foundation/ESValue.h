357b470624c9783bef751b56abf811ca68bf412b
Elf Sundae <elf.sundae@gmail.com>
2015-07-31 23:23:39 +0800

    add comment

--- ESFramework/Core/Foundation/ESValue.h
+++ ESFramework/Core/Foundation/ESValue.h
@@ -11,8 +11,8 @@
 
 /*!
  * Get value from NSNumber or NSString safely.
+ * 如果解析失败，返回对应类型的默认值， 0, 0.0, nil
  */
-
 ES_EXTERN int ESIntValue(id obj);
 ES_EXTERN unsigned int ESUIntValue(id obj);
 ES_EXTERN NSInteger ESIntegerValue(id obj);
@@ -24,13 +24,13 @@ ES_EXTERN unsigned long long ESULongLongValue(id obj);
 ES_EXTERN float ESFloatValue(id obj);
 ES_EXTERN double ESDoubleValue(id obj);
 ES_EXTERN BOOL ESBoolValue(id obj);
-ES_EXTERN NSString *ESStringValue(id obj); // if obj is not NSString or NSNumber, return nil.
-ES_EXTERN NSURL *ESURLValue(id obj); // if obj is not NSString or NSURL, return nil.
+ES_EXTERN NSString *ESStringValue(id obj);
+ES_EXTERN NSURL *ESURLValue(id obj);
 
-/**
- * Deprecated
+/*!
+ * Get value from NSNumber or NSString safely.
+ * 如果解析失败，返回NO并且不改变var的原值
  */
-
 ES_EXTERN BOOL ESIntVal(int *var, id obj);
 ES_EXTERN BOOL ESUIntVal(unsigned int *var, id obj);
 ES_EXTERN BOOL ESIntegerVal(NSInteger *var, id obj);
