53adf3508299246338ae06e4a513697775fcde9c
Elf Sundae <elf.sundae@gmail.com>
2015-09-14 11:13:05 +0800

    Example project

--- Example/App.m
+++ Example/App.m
@@ -18,8 +18,16 @@
         self.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
         self.window.rootViewController = self.rootViewController;
         
+        NSLog(@"%@", [ESApp sharedApp].analyticsInformation);
+        NSLog(@"%@", [ESITunesStoreHelper appLinkForAppID:self.appStoreID storeCountryCode:ESAppStoreCountryCodeChina]);
+        
         [self.window makeKeyAndVisible];
         return YES;
+}
+
+- (NSString *)appStoreID
+{
+        return @"12345678";
 }
 
 @end
