253f2a6d7d9c35f618c4bac87b614b8ee94aa5f3
Elf Sundae <elf.sundae@gmail.com>
2016-12-27 03:50:55 +0800

    Add MacExample app

---
+++ MacExample/MacExample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MacExample
+//
+//  Created by Elf Sundae on 2016/12/27.
+//  Copyright © 2016年 Elf Sundae. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
