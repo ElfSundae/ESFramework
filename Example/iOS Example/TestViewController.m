d5bb509f568edb6e2110cc6a2c68855b266ede90
Elf Sundae <elf.sundae@gmail.com>
2019-04-23 10:58:29 +0800

    Add tests

--- Example/iOS Example/TestViewController.m
+++ Example/iOS Example/TestViewController.m
@@ -9,13 +9,36 @@
 #import "TestViewController.h"
 #import <ESFramework/ESFramework.h>
 
+@interface TestViewController ()
+@property (nonatomic, strong) NSTimer *timer;
+@end
+
 @implementation TestViewController
+
+- (void)dealloc
+{
+    if (self.timer) {
+        [self.timer invalidate];
+    }
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor lightGrayColor];
+
+//    ESWeakSelf;
+//    self.timer = [NSTimer es_scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer *timer) {
+//        [weak_self timerAction:timer];
+//    }];
+
+     self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:[ESWeakProxy proxyWithTarget:self] selector:@selector(timerAction:) userInfo:nil repeats:YES];
+}
+
+- (void)timerAction:(NSTimer *)timer
+{
+    NSLog(@"timer tick: %@", NSDate.date);
 }
 
 @end
