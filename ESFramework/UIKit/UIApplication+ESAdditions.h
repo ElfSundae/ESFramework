430f9b4b31492fc306a898c0209563191e43bc4c
Elf Sundae <elf.sundae@gmail.com>
2019-05-05 20:38:26 +0800

    files order

---
+++ ESFramework/UIKit/UIApplication+ESAdditions.h
@@ -0,0 +1,17 @@
+//
+//  UIApplication+ESAdditions.h
+//  ESFramework
+//
+//  Created by Elf Sundae on 2019/05/05.
+//  Copyright © 2019 www.0x123.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIApplication (ESAdditions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ESFramework/UIKit/UIApplication+ESAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+ESAdditions.m
+//  ESFramework
+//
+//  Created by Elf Sundae on 2019/05/05.
+//  Copyright © 2019 www.0x123.com. All rights reserved.
+//
+
+#import "UIApplication+ESAdditions.h"
+
+@implementation UIApplication (ESAdditions)
+
+@end
