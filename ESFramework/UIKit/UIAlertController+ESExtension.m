0f0acc0d85d1b0bdd79faf00b01ff86cbd22197d
Elf Sundae <elf.sundae@gmail.com>
2019-05-30 17:59:35 +0800

    Add Nullability for .m

--- ESFramework/UIKit/UIAlertController+ESExtension.m
+++ ESFramework/UIKit/UIAlertController+ESExtension.m
@@ -11,48 +11,48 @@
 
 @implementation UIAlertController (ESExtension)
 
-+ (instancetype)actionSheetWithTitle:(NSString *)title message:(NSString *)message
++ (instancetype)actionSheetWithTitle:(nullable NSString *)title message:(nullable NSString *)message
 {
     return [self alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
 }
 
-+ (instancetype)actionSheetWithTitle:(NSString *)title message:(NSString *)message cancelActionTitle:(NSString *)cancelActionTitle
++ (instancetype)actionSheetWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelActionTitle:(nullable NSString *)cancelActionTitle
 {
     UIAlertController *alert = [self actionSheetWithTitle:title message:message];
     [alert addCancelActionWithTitle:cancelActionTitle handler:nil];
     return alert;
 }
 
-+ (instancetype)alertWithTitle:(NSString *)title message:(NSString *)message
++ (instancetype)alertWithTitle:(nullable NSString *)title message:(nullable NSString *)message
 {
     return [self alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
 }
 
-+ (instancetype)alertWithTitle:(NSString *)title message:(NSString *)message cancelActionTitle:(NSString *)cancelActionTitle
++ (instancetype)alertWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelActionTitle:(nullable NSString *)cancelActionTitle
 {
     UIAlertController *alert = [self alertWithTitle:title message:message];
     [alert addCancelActionWithTitle:cancelActionTitle handler:nil];
     return alert;
 }
 
-- (UIAlertAction *)addActionWithTitle:(NSString *)title style:(UIAlertActionStyle)style handler:(void (^)(UIAlertAction *))handler
+- (UIAlertAction *)addActionWithTitle:(nullable NSString *)title style:(UIAlertActionStyle)style handler:(void (^ _Nullable)(UIAlertAction *action))handler
 {
     UIAlertAction *action = [UIAlertAction actionWithTitle:title style:style handler:handler];
     [self addAction:action];
     return action;
 }
 
-- (UIAlertAction *)addDefaultActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *))handler
+- (UIAlertAction *)addDefaultActionWithTitle:(NSString *)title handler:(void (^ _Nullable)(UIAlertAction *action))handler
 {
     return [self addActionWithTitle:title style:UIAlertActionStyleDefault handler:handler];
 }
 
-- (UIAlertAction *)addCancelActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *))handler
+- (UIAlertAction *)addCancelActionWithTitle:(nullable NSString *)title handler:(void (^ _Nullable)(UIAlertAction *action))handler
 {
     return [self addActionWithTitle:title style:UIAlertActionStyleCancel handler:handler];
 }
 
-- (UIAlertAction *)addDestructiveActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *))handler
+- (UIAlertAction *)addDestructiveActionWithTitle:(NSString *)title handler:(void (^ _Nullable)(UIAlertAction *action))handler
 {
     return [self addActionWithTitle:title style:UIAlertActionStyleDestructive handler:handler];
 }
@@ -62,7 +62,7 @@
     [self showAnimated:YES completion:nil];
 }
 
-- (void)showAnimated:(BOOL)animated completion:(void (^)(void))completion
+- (void)showAnimated:(BOOL)animated completion:(void (^ _Nullable)(void))completion
 {
     [UIApplication.sharedApplication presentViewController:self animated:animated completion:completion];
 }
@@ -72,12 +72,12 @@
     [self dismissAnimated:YES completion:nil];
 }
 
-- (void)dismissAnimated:(BOOL)animated completion:(void (^)(void))completion
+- (void)dismissAnimated:(BOOL)animated completion:(void (^ _Nullable)(void))completion
 {
     [self dismissViewControllerAnimated:animated completion:completion];
 }
 
-+ (instancetype)showAlertWithTitle:(NSString *)title message:(NSString *)message cancelActionTitle:(NSString *)cancelActionTitle
++ (instancetype)showAlertWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelActionTitle:(NSString *)cancelActionTitle
 {
     UIAlertController *alert = [self alertWithTitle:title message:message cancelActionTitle:cancelActionTitle];
     [alert show];
