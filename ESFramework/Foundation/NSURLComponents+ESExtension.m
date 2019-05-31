9139c6b59fbcf8734c38643c21ccabce4e9508aa
Elf Sundae <elf.sundae@gmail.com>
2019-05-31 17:24:12 +0800

    do not use ESIs.. to check non-empty

--- ESFramework/Foundation/NSURLComponents+ESExtension.m
+++ ESFramework/Foundation/NSURLComponents+ESExtension.m
@@ -109,7 +109,7 @@
     NSMutableArray *queryItems = [NSMutableArray array];
     for (NSString *_name in dictionary) {
         NSString *name = ESStringValue(_name);
-        if (!ESIsStringWithAnyText(name)) {
+        if (!name || !name.length) {
             continue;
         }
 
