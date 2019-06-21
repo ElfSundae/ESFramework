c18367a159e0b74ded2247783e136d54b6a66aa4
Elf Sundae <elf.sundae@gmail.com>
2019-06-21 20:38:51 +0800

    Create new project, added framework targets and schemes, support Carthage

--- ESFrameworkUI/ESFrameworkUI.h
+++ ESFrameworkUI/ESFrameworkUI.h
@@ -2,9 +2,31 @@
 //  ESFrameworkUI.h
 //  ESFramework
 //
-//  Created by Elf Sundae on 2019/05/20.
+//  Created by Elf Sundae on 2019/04/15.
 //  Copyright © 2019 https://0x123.com. All rights reserved.
 //
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ESFrameworkUI.
+FOUNDATION_EXPORT double ESFrameworkUIVersionNumber;
+
+//! Project version string for ESFrameworkUI.
+FOUNDATION_EXPORT const unsigned char ESFrameworkUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESFrameworkUI/PublicHeader.h>
+
+#if __has_include(<ESFrameworkUI/ESActivityLabel.h>)
+
+#import <ESFrameworkUI/ESActivityLabel.h>
+#import <ESFrameworkUI/ESBadgeView.h>
+#import <ESFrameworkUI/ESErrorView.h>
+#import <ESFrameworkUI/ESRefreshControl.h>
+#import <ESFrameworkUI/ESRefreshControlDefaultContentView.h>
+#import <ESFrameworkUI/ESStatusOverlayView.h>
+#import <ESFrameworkUI/ESTableViewController.h>
+
+#else
 
 #import "ESActivityLabel.h"
 #import "ESBadgeView.h"
@@ -13,3 +35,5 @@
 #import "ESRefreshControlDefaultContentView.h"
 #import "ESStatusOverlayView.h"
 #import "ESTableViewController.h"
+
+#endif
