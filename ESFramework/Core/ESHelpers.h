da935d5c17419816e5d598e323692fbde8d67578
Elf Sundae <elf.sundae@gmail.com>
2019-08-20 01:48:02 +0800

    #import <Foundation/Foundation.h>

--- ESFramework/Core/ESHelpers.h
+++ ESFramework/Core/ESHelpers.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 https://0x123.com. All rights reserved.
 //
 
-#import <TargetConditionals.h>
+#import <Foundation/Foundation.h>
 #import <CoreGraphics/CoreGraphics.h>
 #if TARGET_OS_IOS || TARGET_OS_TV
 #import <UIKit/UIKit.h>
